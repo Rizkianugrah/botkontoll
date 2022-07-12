4a642df4886fe5e4d1b80d9bdd76282d48fbff05
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-12 22:36:16 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,17 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi 」
 │ • ${global.donasi}
+.┌─「 Donasi • Pulsa 」
+
+│ • Telkomsel: [081387089252]
+
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+
+│ • Ovo: [083199789390]
+
+❏────
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
